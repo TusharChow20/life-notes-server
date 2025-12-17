ca12feb6d904471344d5d0316891882d1c83bf19
Tushar Chowdhury <tusharchowdhury20211@gmail.com>
2025-12-17 14:09:48 +0600

    user data send to database

--- index.js
+++ index.js
@@ -22,7 +22,19 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const myDB = client.db("LifeNotes");
+    const userCollection = myDB.collection("userInfo");
     // Send a ping to confirm a successful connection
+    //###############-------user api----###############
+
+    app.post("/userInfo", async (req, res) => {
+      const newUser = req.body;
+      const result = await userCollection.insertOne(newUser);
+      console.log(newUser);
+
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       `Pinged your deployment. You successfully connected to MongoDB! ${port}`
